3668044ba0f41de04d0681ab47243aa7ae7c31e9
Abhilash G <abhilash.ag1997@gmail.com>
2017-10-03 17:50:18 +0530

    fix incorrect class name declarations

--- Black.java
+++ Black.java
@@ -14,7 +14,7 @@ class Black {
     }
     public static void main(String args[]){
         Black o = new Black(5);
-        Black i = new Black(2, 4, 6);
+        black i = new Black(2, 4, 6);
         o.print();
         i.print();
     }
