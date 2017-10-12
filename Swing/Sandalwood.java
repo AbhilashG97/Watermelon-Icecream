90a4dc73e3e3060cc4bf69af6f187b2ff8015cb4
Abhilash G <abhilash.ag1997@gmail.com>
2017-10-12 22:53:47 +0530

    added Swing programs

---
+++ Swing/Sandalwood.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Sandalwood extends JFrame{
+
+	JFrame j;
+	JFrame onClick;
+	final JTextField tf;
+
+	public Sandalwood(){
+		JButton button = new JButton("Click Me !");
+		tf = new JTextField();
+		tf.setBounds(50, 50, 150, 20);
+		button.setBounds(130, 100, 100, 40);
+		add(button);
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				onClick = new JFrame("Hello world");
+				onClick.add(tf);
+				tf.setText("Hello !");
+				onClick.setSize(400, 400);
+				onClick.setLayout(null);
+				onClick.setVisible(true);
+			} 
+		});
+		setSize(400, 500);
+		setLayout(null);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Sandalwood();
+	}
+}
