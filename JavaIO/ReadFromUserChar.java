e220e0cf2af19ac3c0f793a1d479a849d6b543a6
Abhilash G <abhilash.ag1997@gmail.com>
2017-10-12 22:52:12 +0530

    Added Java IO Programs

---
+++ JavaIO/ReadFromUserChar.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class ReadFromUserChar extends ReadFromUser{
+
+	private FileReader readInput;
+	private FileWriter writeOutput;
+	private String userInput;
+
+	public ReadFromUserChar() throws IOException{
+		readInput = new FileReader("ghost.txt");
+		writeOutput = new FileWriter("poltergeist.txt", true);
+		userInput = null;
+	}
+
+	public void readFromUser() throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		userInput = bfr.readLine();
+		bfr.close();
+	}
+
+	@Override
+	public void writeToFile() throws IOException{
+		writeOutput.write(userInput);
+	}	
+
+	public void readAndWriteFromFile() throws IOException{
+		int c = 0;
+		while((c = readInput.read())!= -1){
+			writeOutput.write(c);
+		}
+	}
+
+	public void closeAll() throws IOException{
+		readInput.close();
+		writeOutput.close();
+	}	
+
+}
