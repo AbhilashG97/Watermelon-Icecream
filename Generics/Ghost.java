907bbdcd0b806801ce17246a8320472ea9553bc5
AbhilashG97 <abhilash.ag1997@gmail.com>
2017-10-16 11:18:38 +0530

    added new programs

---
+++ Generics/Ghost.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Ghost {
+	public static void main(String[] args) {
+		List<String> ls = new ArrayList<>();
+		List<Integer> li = new ArrayList<>();
+		System.out.println(li.getClass() == ls.getClass());
+		// It prints true, because at runtime both of them return 
+		// the same class due to tyoe erasure
+	} 
+}
---
+++ Generics/Meow.java
@@ -0,0 +1,10 @@
+public class Meow {
+
+	private String catName;
+	private String dogName;
+
+	public Meow(){
+		// Default Constructor
+	}
+
+}
