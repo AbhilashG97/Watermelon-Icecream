ba8713cc0c8d0037578bf7781255d33f74e28565
Abhilash G <abhilash.ag1997@gmail.com>
2017-11-20 18:41:56 +0530

    more concurrency programs

--- Concurrency/Kinkade.java
+++ Concurrency/Kinkade.java
@@ -1,7 +1,49 @@
+import java.io.*;
+import java.util.ArrayList;
 
+public class Kinkade {
 
-public class kinkade {
+	public static class MyThread {
 
+		private ArrayList<String> arr;
 
+		public MyThread(){
+			arr = new ArrayList<>();
+		}
 
+		Thread t1 = new Thread() {
+
+			@Override
+			public void run(){
+
+				for(int i=0; i<10; i++){
+					arr.add(i+" ");
+				}						
+
+			} 
+		};
+
+		Thread t2 = new Thread(){
+
+			@Override
+			public void run(){
+				System.out.print(arr);
+				// for(String i : arr){
+				// 	System.out.print(i+" ");
+				// }
+			}
+
+		};
+
+		public void startAllThreads(){
+			t1.start();
+			t2.start();
+		}
+	}
+
+	public static void main(String[] args) {
+		Kinkade.MyThread blah = new Kinkade.MyThread();
+		blah.startAllThreads();
+		System.out.println(System.currentTimeMillis());
+	}	
 }
