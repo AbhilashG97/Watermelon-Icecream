2e91b97fde8c84526ae65c9432d5aa7f3ce6f7f3
Abhilash G <abhilash.ag1997@gmail.com>
2017-12-29 18:28:03 +0530

    Adding Frames

---
+++ Swing/Hum.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Hum {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	}
+}
