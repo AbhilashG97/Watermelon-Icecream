bb4e1e7449bbd9424b57e599d8f5fe8b03769586
Abhilash G <abhilash.ag1997@gmail.com>
2018-10-13 01:41:38 +0530

    JavaFX: Hello, World!

---
+++ JavaFX/HelloWatermelonyWorld/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Scene scene = new Scene(root,600,400);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Hello, world!");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
