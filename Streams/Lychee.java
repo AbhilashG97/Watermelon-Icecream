425fddad64340f69a25405cb6d3eac04a60e8c7e
AbhilashG97 <abhilash.ag1997@gmail.com>
2018-11-12 22:24:06 +0530

    More Stream API stuff

--- Streams/Lychee.java
+++ Streams/Lychee.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 public class Lychee {
 
+    /**
+     * 
+     * @param a Stream of String
+     * @return ArrayList<Integer>
+     */
     public ArrayList<Integer> sortEvenNumbers(ArrayList<String> stringNumberList) {
         return stringNumberList
         .stream()
@@ -10,6 +15,26 @@ public class Lychee {
         .filter((number) -> number%2==0)
         .collect(Collectors.toCollection(ArrayList::new));
     } 
+
+    /**
+     * 
+     * @param integerList A stream of integers
+     * @return Stream<Integer>
+     */
+    public Stream<Integer> getOddIntegerStream(ArrayList<Integer> integerList) {
+        return integerList.stream()
+                          .filter((number) -> number%2!=0);
+    }
+
+
+    /**
+     * 
+     * @param integerList 
+     * @return Optional<Stream>
+     */
+    public Optional<Integer> getMultiplesOfThreeOptional(ArrayList<Integer> integerList) {
+        return integerList.stream().findAny();
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,6 +52,21 @@ public class Lychee {
         System.out.println("\nEven numbers after sorting are :-\n");
         new Lychee().sortEvenNumbers(numberList).forEach(System.out::println);;
 
+        ArrayList<Integer> integerList = numberList.stream()
+                                                   .map((number) -> Integer.valueOf(number))
+                                                   .collect(Collectors.toCollection(ArrayList::new));
+        
+        System.out.println("\nAnother set of even numbers are -\n");
+
+        new Lychee().getOddIntegerStream(integerList)
+                    .collect(Collectors.toCollection(ArrayList::new))
+                    .forEach(System.out::println);
+        // Fix this
+        new Lychee().getMultiplesOfThreeOptional(integerList)
+                    .stream()
+                    .collect(Collectors.toCollection(ArrayList<Integer>::new))
+                    .forEach(System.out::println);
+                                            
         numberScanner.close();
         scanner.close();
     }
