14e843e5f275615d519b469b6062b7e959d43408
AbhilashG97 <abhilash.ag1997@gmail.com>
2019-04-29 20:04:45 +0530

    JavaFX: MVC ChoiceBox and ComboBox

--- JavaFX/WatermelonDessertSelector/src/watermelondessertselector/WatermelonDessertSelectorController.java
+++ JavaFX/WatermelonDessertSelector/src/watermelondessertselector/WatermelonDessertSelectorController.java
@@ -10,6 +10,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -21,6 +23,7 @@ import javafx.scene.control.TextField;
  */
 public class WatermelonDessertSelectorController implements Initializable {
     
+    // Watermelon Dessert Selector
     @FXML private RadioButton watermelonIcecubesRadioButton;
     @FXML private RadioButton thinWatermelonShredsRadioButton;
     @FXML private RadioButton watermelonBallsRadioButton;
@@ -28,19 +31,32 @@ public class WatermelonDessertSelectorController implements Initializable {
     @FXML private Button orderButton;
     @FXML private Label orderSummaryLabel;
    
+    // Add Extra Fruits
+    @FXML private ChoiceBox extraFruitsChoiceBox;
+    @FXML private Button addFruitButton;
+    @FXML private Label extraFruitSummaryLabel;
+    
+    // Add Dried Fruits
+    @FXML private ComboBox driedFruitsComboBox;
+    @FXML private Label driedFruitSummaryLabel;
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         onOrderButtonPressed();
+        initializeChoiceBoxData();
+        initializeComboBoxData();
+        onAddFruitButtonClicked();
     }    
     
+    /**
+     * Invoked when the Order button of
+     * the Watermelon Dessert Selector is pressed
+     */
     public void onOrderButtonPressed() {
         orderButton.setOnAction((event) -> {
-            
             String order = "";
             
             if(watermelonIcecubesRadioButton.isSelected()) {
@@ -61,5 +77,30 @@ public class WatermelonDessertSelectorController implements Initializable {
             orderSummaryLabel.setText(order);
             System.out.println(order);
         });
+    }
+    
+    public void onAddFruitButtonClicked() {
+        addFruitButton.setOnAction((event) -> {
+            extraFruitSummaryLabel.setText(extraFruitsChoiceBox.getValue()
+                    .toString());
+            System.out.println(extraFruitsChoiceBox.getValue()
+                    .toString());
+        });
+    }
+    
+    public void onComboBoxItemSelected() {
+        driedFruitSummaryLabel.setText("Selected Dry Fruit:\n"
+                +driedFruitsComboBox.getValue().toString());
+        System.out.println(driedFruitsComboBox.getValue().toString());
+    }
+    
+    private void initializeChoiceBoxData() {
+        extraFruitsChoiceBox.getItems().add("Lychee");
+        extraFruitsChoiceBox.getItems().addAll("Apple", "Mango", "Orange");
+    }
+    
+    private void initializeComboBoxData() {
+        driedFruitsComboBox.getItems().add("Raisin");
+        driedFruitsComboBox.getItems().addAll("Nut", "Almond", "Prune");
     }
 }
